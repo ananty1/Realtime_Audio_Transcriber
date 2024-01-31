e10aa221e9eb5934f220336e625dae9d9e2b1713
anant <ananty@iitbhilai.ac.in>
2024-01-31 18:41:17 +0530

    Working RealTime Transcriber

--- transcriber_webpage/consumers.py
+++ transcriber_webpage/consumers.py
@@ -5,23 +5,46 @@ from deepgram import Deepgram
 from typing import Dict
 import os 
 load_dotenv()
+import json 
+import time
+
 
 class TranscriptConsumer(AsyncWebsocketConsumer):
         dg_client = Deepgram(os.getenv('DEEPGRAM_API_KEY'))
-
+        
+        start_time = 0
+        
         async def get_transcript(self, data: Dict) -> None:
             if 'channel' in data:
                 transcript = data['channel']['alternatives'][0]['transcript']
+                accuracy = data['channel']['alternatives'][0]['confidence']
+                audio_time = data['start']+data['duration']
 
                 if transcript:
-                    await self.send(transcript)
+                    latency = time.time()-audio_time-self.start_time
+
+                    response_data = {
+                        'transcript': transcript,
+                        'accuracy': accuracy,
+                        'latency':latency
+                    }
+                    response_json = json.dumps(response_data)
+                    await self.send(response_json)
 
 
         async def connect_to_deepgram(self):
             try:
+                self.start_time=time.time()
+                connection_start = time.time()
                 self.socket = await self.dg_client.transcription.live({'punctuate': True, 'interim_results': False})
+                connection_end = time.time()
+                connection_time= connection_end-connection_start
+                print("Startime:",self.start_time,"connection time:",connection_time)
+                self.start_time = self.start_time+connection_time
+                print("Update start time",self.start_time)
                 self.socket.registerHandler(self.socket.event.CLOSE, lambda c: print(f'Connection closed with code {c}.'))
                 self.socket.registerHandler(self.socket.event.TRANSCRIPT_RECEIVED, self.get_transcript)
+                
 
             except Exception as e:
                 raise Exception(f'Could not open socket: {e}')
@@ -41,4 +64,3 @@ class TranscriptConsumer(AsyncWebsocketConsumer):
 
         async def receive(self, bytes_data):
             self.socket.send(bytes_data)
-
